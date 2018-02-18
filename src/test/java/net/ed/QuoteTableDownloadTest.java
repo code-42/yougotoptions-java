297fc6defb39f8a4b71ffb67f50cd3db8f77af55
Edward Dupre <edwd42@gmail.com>
2018-02-17 23:49:01 -0500

    add Junit test framework

---
+++ src/test/java/net/ed/QuoteTableDownloadTest.java
@@ -0,0 +1,13 @@
+package net.ed;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuoteTableDownloadTest {
+
+    @Test
+    public void main() {
+        fail("test not implemented yet");
+    }
+}
---
+++ src/test/java/net/ed/ReadQuoteDataTest.java
@@ -0,0 +1,13 @@
+package net.ed;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReadQuoteDataTest {
+
+    @Test
+    public void countLines() {
+        fail("test not implemented yet");
+    }
+}
